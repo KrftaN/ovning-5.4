39eb178850fbcb275309fa538a557db99f8a65ae
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-05-31 13:56:58 +0200

    Add project files.

---
+++ ovning 5.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace övning_5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = { "Anna", "Kalle", "Erik", "Maria", "Bertil" };
+
+            string förstaNamn = namn[0];
+
+            for (int i = 1; i < namn.Length; i++)
+            {
+                if (string.Compare(namn[i], förstaNamn, StringComparison.OrdinalIgnoreCase) < 0)
+                {
+                    förstaNamn = namn[i];
+                }
+            }
+
+            Console.WriteLine($"Namnet som kommer först i bokstavsordning är: {förstaNamn}");
+        }
+    }
+}
